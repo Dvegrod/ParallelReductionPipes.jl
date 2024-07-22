e5547103c414e2e429569f1e21ed268d9c9e6a5f
Dvegrod <dvegrodu@gmail.com>
2024-07-22 17:43:35 +0200

    Create red_test_show.jl

---
+++ red_test_show.jl
@@ -0,0 +1,25 @@
+
+using ADIOS2
+using Plots
+
+include("src/reducer.jl")
+
+adios = adios_init_serial()
+io = declare_io(adios, "IO")
+engine = open(io, "reducer-o.bp", mode_readRandomAccess)
+
+y = inquire_variable(io, "out")
+
+buf = Array{Float64}(undef, 100,100)
+
+get(engine, y, buf)
+
+spy(buf)
+
+savefig("outred.png")
+
+#@assert reducer.@check_for_val_w_timeout((f() = reducer._get(io, engine, :exec_ready)), 1, 10)
+
+# Build
+#@assert reducer.@check_for_val_w_timeout((f() = reducer._get(io, engine, :exec_ready)), 2, 10)
+
