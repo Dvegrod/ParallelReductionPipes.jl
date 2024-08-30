02415d43faee04e4b65ed632081be4c2150576aa
Daniel Sergio Vega Rodriguez <dvegarod@daint101.login.cscs.ch>
2024-08-30 10:48:42 +0200

    Remove trash

--- src/runtime.jl
+++ src/runtime.jl
@@ -1,17 +1,12 @@
 
-using Pkg
 
 if ARGS[1] == "CPU"
-    Pkg.add("MPI")
-
     using MPI
     using ParallelReductionPipes
 
     @info pwd()
     ParallelReductionPipes.main(ParallelReductionPipes.CPUBackend)
 else
-    Pkg.add("MPI")
-    Pkg.add("CUDA")
     using CUDA
     using MPI
 
